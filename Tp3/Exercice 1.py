8ea4fe3e14d75201d536472777cf3f33fdda920c
abderrahmane02tahiri <abderrahmane.tahiri.02@gmail.com>
2023-10-31 21:54:53 +0100

    update the sum fonction

--- Tp3/Exercice 1.py
+++ Tp3/Exercice 1.py
@@ -1,18 +1,19 @@
 import math
+
+
 def somme(m, n):
     total = 0
-    if m < n :
-        for i in range(m , n + 1):
+    if m < n:
+        for i in range(m, n + 1):
             total += i
-
-    else :
-        for i in range(n , m + 1):
+    else:
+        for i in range(n, m + 1):
             total += i
-
     return total
 
+
 def delta(a, b, c):
-    return b**2 - 4*a*c
+    return b ** 2 - 4 * a * c
 
 def NombreRacine(a, b, c):
     D = delta(a, b, c)
@@ -32,17 +33,19 @@ def AfficheNombreRacine(a, b, c):
     else:
         print("Il n'y a pas de solution réelle.")
 
+
 def Racine1(a, b, c):
     D = delta(a, b, c)
     if D >= 0:
-        return (-b + math.sqrt(D)) / (2*a)
+        return (-b + math.sqrt(D)) / (2 * a)
     else:
         return None
+
 
 def Racine2(a, b, c):
     D = delta(a, b, c)
     if D >= 0:
-        return (-b - math.sqrt(D)) / (2*a)
+        return (-b - math.sqrt(D)) / (2 * a)
     else:
         return None
 
@@ -50,14 +53,17 @@ def Racine2(a, b, c):
 def conversion_temps(h, m, s):
     return h * 3600 + m * 60 + s
 
+
 def temps_ecoule(h1, m1, s1, h2, m2, s2):
     temps1 = conversion_temps(h1, m1, s1)
     temps2 = conversion_temps(h2, m2, s2)
     duree = abs(temps1 - temps2)
     return duree
 
+
 def conversion_distance(km, m, cm):
     return km * 1000 + m + cm / 100
+
 
 def vitesse(distance, temps):
     distance_en_metres = conversion_distance(*distance)
